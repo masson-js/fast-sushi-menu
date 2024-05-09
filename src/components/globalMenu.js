2b52b467d447f79d04ddc972df8c34775c9b035a
Vladimir Nikulin <massonn.js@gmail.com>
2024-05-09 18:13:05 +0200

    two

--- src/components/globalMenu.js
+++ src/components/globalMenu.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Sushi from "./sushi";
 import SetsSection from "./sets";
 import PhiladelphiaSection from "./philadelphiaS";
@@ -20,28 +20,19 @@ export default function GlobalMenu() {
   const [philadelphiaList, setPhiladelphiaList] = useState({});
   const [californiaList, setCaliforniaList] = useState({});
 
-  const windowRef = useRef(null);
-
   useEffect(() => {
-    if (CaliforniaClicked && windowRef.current) {
-      windowRef.current.scrollIntoView({ behavior: "smooth", block: 'start' });
-      
-    }
-    if (PhiladelphiaClicked && windowRef.current) {
-      windowRef.current.scrollIntoView({ behavior: "smooth", block: 'start' });
-    }
-
     const california = JSON.parse(JSON.stringify(californiaArray));
     setCaliforniaList(california);
-    
+
     const philadelphia = JSON.parse(JSON.stringify(philadelphiaArray));
     setPhiladelphiaList(philadelphia);
+    
     const sushi = JSON.parse(JSON.stringify(sushiArray));
     setSushiList(sushi);
 
     const sets = JSON.parse(JSON.stringify(setsArray));
     setSetsList(sets);
-  }, [CaliforniaClicked, PhiladelphiaClicked]);
+  }, []);
 
   return (
     <>
@@ -49,11 +40,10 @@ export default function GlobalMenu() {
         <button
           className="menu-button sushiButton"
           onClick={() => setSushiClicked(!sushiClicked)}
-          ref={windowRef}
         >
           Sushi
         </button>
-        {sushiClicked && <Sushi sushiList={sushiList} />}
+        <div>{sushiClicked && <Sushi sushiList={sushiList} />}</div>
 
         <button
           className="menu-button setsButton"
@@ -61,19 +51,17 @@ export default function GlobalMenu() {
         >
           Sets
         </button>
-        {setsClicked && <SetsSection setsList={setsList} />}
+        <div>{setsClicked && <SetsSection setsList={setsList} />}</div>
         <button
           className="menu-button philadelphiaButton"
           onClick={() => setPhiladelphiaClicked(!PhiladelphiaClicked)}
-          
         >
           Philadelphia
         </button>
-        <div ref={windowRef}>
-
-        {PhiladelphiaClicked && (
-          <PhiladelphiaSection philadelphiaList={philadelphiaList} />
-        )}
+        <div>
+          {PhiladelphiaClicked && (
+            <PhiladelphiaSection philadelphiaList={philadelphiaList} />
+          )}
         </div>
         <button
           className="menu-button californiaButton"
@@ -81,7 +69,7 @@ export default function GlobalMenu() {
         >
           California
         </button>
-        <div ref={windowRef}>
+        <div>
           {CaliforniaClicked && (
             <CaliforniaSection californiaList={californiaList} />
           )}
